f4225545f7108b6fb9ae32095753ae74bf53e090
29usama <muhammadusama2117@gmail.com>
2024-03-31 21:18:56 +0500

    im tired

--- network/vpc1.tf
+++ network/vpc1.tf
@@ -13,10 +13,16 @@ variable "vpc1_name" {
   default = "vpc1-ilmiya"
 }
 
-resource "google_compute_network" "vpc1_network" {
-  name                    = var.vpc1_name
-  auto_create_subnetworks = false
+data "google_compute_network" "existing_vpc1" {
+  count = var.vpc1_name != "" ? 1 : 0
+  name  = var.vpc1_name
+}
 
+resource "google_compute_network" "vpc1_network" {
+  count = var.vpc1_name != "" ? 1 : 0
+  name  = var.vpc1_name
+
+  auto_create_subnetworks = false
   lifecycle {
     ignore_changes = [name]
   }
--- network/vpc2.tf
+++ network/vpc2.tf
@@ -2,10 +2,16 @@ variable "vpc2_name" {
   default = "vpc2-ilmiya"
 }
 
-resource "google_compute_network" "vpc2_network" {
-  name                    = var.vpc2_name
-  auto_create_subnetworks = false
+data "google_compute_network" "existing_vpc2" {
+  count = var.vpc2_name != "" ? 1 : 0
+  name  = var.vpc2_name
+}
 
+resource "google_compute_network" "vpc2_network" {
+  count = var.vpc2_name != "" ? 1 : 0
+  name  = var.vpc2_name
+
+  auto_create_subnetworks = false
   lifecycle {
     ignore_changes = [name]
   }
