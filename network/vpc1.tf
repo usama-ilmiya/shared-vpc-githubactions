3effe46a711427c704e0feaf45b6f4153e52f51b
29usama <muhammadusama2117@gmail.com>
2024-03-31 20:47:37 +0500

    make change in vpc files

--- network/vpc1.tf
+++ network/vpc1.tf
@@ -31,7 +31,12 @@ variable "vpc1_name" {
   default = "vpc1-ilmiya"
 }
 
+data "google_compute_network" "existing_vpc1" {
+  name = var.vpc1_name
+}
+
 resource "google_compute_network" "vpc1_network" {
+  count                   = data.google_compute_network.existing_vpc1.id != "" ? 0 : 1
   name                    = var.vpc1_name
   auto_create_subnetworks = false
 
--- network/vpc2.tf
+++ network/vpc2.tf
@@ -29,7 +29,12 @@ variable "vpc2_name" {
   default = "vpc2-ilmiya"
 }
 
+data "google_compute_network" "existing_vpc2" {
+  name = var.vpc2_name
+}
+
 resource "google_compute_network" "vpc2_network" {
+  count                   = data.google_compute_network.existing_vpc2.id != "" ? 0 : 1
   name                    = var.vpc2_name
   auto_create_subnetworks = false
 
