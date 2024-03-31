5ef3412213f868b4febc9e3f74501ac5f4c2fba3
29usama <muhammadusama2117@gmail.com>
2024-03-31 21:31:29 +0500

    hah

--- network/vpc1.tf
+++ network/vpc1.tf
@@ -14,17 +14,16 @@ variable "vpc1_name" {
 }
 
 data "google_compute_network" "existing_vpc1" {
-  count = var.vpc1_name != "" ? 1 : 0
-  name  = var.vpc1_name
+  name = var.vpc1_name
 }
 
 resource "google_compute_network" "vpc1_network" {
-  count = var.vpc1_name != "" ? 1 : 0
-  name  = var.vpc1_name
-
+  name                    = var.vpc1_name
   auto_create_subnetworks = false
+
   lifecycle {
     ignore_changes = [name]
   }
+
+  depends_on = [data.google_compute_network.existing_vpc1]
 }
- 
--- network/vpc2.tf
+++ network/vpc2.tf
@@ -3,16 +3,16 @@ variable "vpc2_name" {
 }
 
 data "google_compute_network" "existing_vpc2" {
-  count = var.vpc2_name != "" ? 1 : 0
-  name  = var.vpc2_name
+  name = var.vpc2_name
 }
 
 resource "google_compute_network" "vpc2_network" {
-  count = var.vpc2_name != "" ? 1 : 0
-  name  = var.vpc2_name
-
+  name                    = var.vpc2_name
   auto_create_subnetworks = false
+
   lifecycle {
     ignore_changes = [name]
   }
+
+  depends_on = [data.google_compute_network.existing_vpc2]
 }
