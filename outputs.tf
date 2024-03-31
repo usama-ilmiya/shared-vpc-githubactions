0033fc0dcb6e3ded5bb5ace37a0515d3064aff90
29usama <muhammadusama2117@gmail.com>
2024-03-31 16:38:46 +0500

    correcting vpc names

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vpc1_network_name" {
-  value = module.vpc1.vpc_network_name
+  value = module.vpc1.vpc1_network_name
 }
 
 output "vpc2_network_name" {
-  value = module.vpc2.vpc_network_name
+  value = module.vpc2.vpc2_network_name
 }
