e43b24f770336748311a8355567f160c2443c742
29usama <muhammadusama2117@gmail.com>
2024-03-31 22:51:25 +0500

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,11 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project = "gmzdev"
+  region  = "us-central1"
 }
 
-resource "google_compute_network" "shared_vpc" {
-  name = var.shared_vpc_name
+resource "google_compute_network" "shared_network" {
+  name                    = "NETWORK_NAME"
+  auto_create_subnetworks = false
 }
 
-# Define your other required VPC resources here
-
-# Example subnet creation
-resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet-1"
-  ip_cidr_range = "10.0.0.0/24"
-  network       = google_compute_network.shared_vpc.self_link
-  region        = var.region
-}
-
-# Add more resources as needed
+# Define other resources like subnets, firewall rules, etc.
--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "shared_vpc_self_link" {
-  value = google_compute_network.shared_vpc.self_link
-}
-
-# Define other outputs as needed
